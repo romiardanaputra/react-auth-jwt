6180965d7e07a6e384b43a320b62ef8e11407aed
romiardanaputra <romiardana21@gmail.com>
2024-08-07 20:46:29 +0800

    refactor(boiler plate): Add config preline tailwind UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "node_modules/preline/dist/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [ require('preline/plugin'),],
 };

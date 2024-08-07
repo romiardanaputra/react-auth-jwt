aabb0077d9232233a008e5128350001646030425
romiardanaputra <romiardana21@gmail.com>
2024-08-07 21:51:41 +0800

    feat(login): Add login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import LoginForm from "../components/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <>
+      <div>
+        <h1>Login Form</h1>
+        <LoginForm />
+      </div>
+    </>
+  );
+};
+
+export default LoginPage;

6025a215b49ec21d7f74dd82712f8d49528cc337
romiardanaputra <romiardana21@gmail.com>
2024-08-08 15:25:59 +0800

    feat(login): Add login form component

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -5,21 +5,28 @@ import CustomInputField from "./ui/input/CustomInputField";
 const LoginForm = () => {
   return (
     <>
-      <div>
-        <form>
-          <CustomInputField
-            inputType="email"
-            placeholder="Email"
-            name="email"
-          />
+      <div className="min-h-screen max-w-md mx-auto  relative ">
+        <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-full space-y-6">
+          <div>
+            <h1 className="text-3xl font-bold">Login Form</h1>
+          </div>
+          <form action="" className="space-y-4">
+            <CustomInputField
+              inputType="email"
+              placeholder="Email"
+              name="email"
+              labelName="Email Address"
+            />
 
-          <CustomInputField
-            inputType="password"
-            placeholder="password"
-            name="password"
-          />
-          <Button variant={ButtonVariantEnum.SOLID} btnText="Login" />
-        </form>
+            <CustomInputField
+              inputType="password"
+              placeholder="password"
+              name="password"
+              labelName="Password"
+            />
+            <Button variant={ButtonVariantEnum.OUTLINE} btnText="Login Now" />
+          </form>
+        </div>
       </div>
     </>
   );

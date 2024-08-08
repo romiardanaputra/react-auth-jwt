5c9759b6c6ec482f2b68601f66d24ae857659034
romiardanaputra <romiardana21@gmail.com>
2024-08-08 15:26:13 +0800

    feat(register): Add register form component

---
+++ src/components/RegisterForm.tsx
@@ -0,0 +1,35 @@
+import Button from "./ui/button";
+import ButtonVariantEnum from "./ui/button/index.enum";
+import CustomInputField from "./ui/input/CustomInputField";
+
+const RegisterForm = () => {
+  return (
+    <>
+      <div className="min-h-screen max-w-md mx-auto  relative ">
+        <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-full space-y-6">
+          <div>
+            <h1 className="text-3xl font-bold">Register Form</h1>
+          </div>
+          <form action="" className="space-y-4">
+            <CustomInputField
+              inputType="email"
+              placeholder="Email"
+              name="email"
+              labelName="Email Address"
+            />
+
+            <CustomInputField
+              inputType="password"
+              placeholder="password"
+              name="password"
+              labelName="Password"
+            />
+            <Button variant={ButtonVariantEnum.OUTLINE} btnText="Register" />
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RegisterForm;

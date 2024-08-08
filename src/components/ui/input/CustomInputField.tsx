1ded2f33e108c9c250fbed470e04f47db4f3a4c5
romiardanaputra <romiardana21@gmail.com>
2024-08-08 15:26:55 +0800

    chore(custom input): Fix custom input

--- src/components/ui/input/CustomInputField.tsx
+++ src/components/ui/input/CustomInputField.tsx
@@ -1,15 +1,11 @@
-type CustomInputFieldProps = {
-  inputType: string;
-  placeholder: string;
-  name: string;
-  value?: string;
-  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-};
+import { CustomInputFieldProps } from "./index.props";
+
 const CustomInputField = ({
   inputType,
   placeholder,
   name,
   value,
+  labelName,
   onChange,
 }: CustomInputFieldProps) => {
   return (
@@ -17,29 +13,19 @@ const CustomInputField = ({
       <div className="relative">
         <input
           type={inputType}
-          name={name}
-          value={value}
-          onChange={onChange}
-          className="peer py-3 pe-0 ps-8 block w-full bg-transparent border-t-transparent border-b-2 border-x-transparent border-b-gray-200 text-sm focus:border-t-transparent focus:border-x-transparent focus:border-b-blue-500 focus:ring-0 disabled:opacity-50 disabled:pointer-events-none dark:border-b-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600 dark:focus:border-b-neutral-600"
+          id="hs-floating-input-email"
+          className="peer p-4 block w-full border-gray-200 rounded-lg text-sm placeholder:text-transparent focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:focus:ring-neutral-600 focus:pt-6 focus:pb-2 [&:not(:placeholder-shown)]:pt-6 [&:not(:placeholder-shown)]:pb-2 autofill:pt-6 autofill:pb-2"
           placeholder={placeholder}
+          value={value}
+          name={name}
+          onChange={onChange}
         />
-        <div className="absolute inset-y-0 start-0 flex items-center pointer-events-none ps-2 peer-disabled:opacity-50 peer-disabled:pointer-events-none">
-          <svg
-            className="shrink-0 size-4 text-gray-500 dark:text-neutral-500"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
-            <circle cx="12" cy="7" r="4"></circle>
-          </svg>
-        </div>
+        <label
+          htmlFor="hs-floating-input-email"
+          className="absolute top-0 start-0 p-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent  origin-[0_0] dark:text-white peer-disabled:opacity-50 peer-disabled:pointer-events-none peer-focus:scale-90 peer-focus:translate-x-0.5 peer-focus:-translate-y-1.5 peer-focus:text-gray-500 dark:peer-focus:text-neutral-500 peer-[:not(:placeholder-shown)]:scale-90 peer-[:not(:placeholder-shown)]:translate-x-0.5 peer-[:not(:placeholder-shown)]:-translate-y-1.5 peer-[:not(:placeholder-shown)]:text-gray-500 dark:peer-[:not(:placeholder-shown)]:text-neutral-500 "
+        >
+          {labelName}
+        </label>
       </div>
     </>
   );
